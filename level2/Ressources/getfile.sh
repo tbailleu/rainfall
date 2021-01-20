5b4fab078b3ec5875e4b7dc81b4e54261ff1cc48
Laura BAILLEUL <tbailleu@student.42.fr>
2021-01-20 10:53:05 +0100

    Delete duplicated file

--- level2/Ressources/getfile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-dir=$(dirname $0)
-rm $dir/level2
-echo -e "\e[91mPassword for level2 :\e[92m 53a4a712787f40ec66c3c26c1f4b164dcad5552b038bb0addd69bf5bf6fa8e77 \e[0m"
-scp level2@rain:level2 $dir/level2
